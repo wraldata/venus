a6ab6c13d87d434980f151e1f71e7981789c4341
mtdukes <tyler.dukes@gmail.com>
2016-05-20 17:51:45 -0400

    stupid

--- leg_tracker/updateBillNo.py
+++ leg_tracker/updateBillNo.py
@@ -43,7 +43,7 @@ for vote in rollcall_data:
 
 	#update the rollcall data
 	pk_id = vote['url'].split('/')[-2]
-	console.log(pk_id)
+	print pk_id
 	#_, created = Rollcall.objects.update_or_create(
 	#	rollcall_id = pk_id,
 	#	defaults = updated_values
